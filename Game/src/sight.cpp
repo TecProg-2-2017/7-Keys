8a2dc8b1278b8b877c6048057670a131fc9ec2b2
Arthur Assis <arthur120496@gmail.com>
2017-08-27 16:06:03 -0300

    Applying stylesheet

--- Game/src/sight.cpp
+++ Game/src/sight.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
 #include <core/animation.h>
 #include <core/environment.h>
-
 #include "sight.h"
 
-Sight::Sight(Object *parent, ObjectID id, double x, double y, double w, double h)
-    : Object(parent, id, x, y, w, h)
-{
+Sight::Sight(Object *parent, ObjectID id, double x, double y, double w,
+  double h):Object(parent, id, x, y, w, h){
 }
 
-Sight::~Sight()
-{
+Sight::~Sight(){
 }
 
 //Alterar
-void
-Sight::draw_self()
-{
+void Sight::draw_self(){
 	// Environment *env = Environment::get_instance();
 	// Rect rect {x(),y(),w(),h()};
 	// env->canvas->draw(rect, Color::WHITE);
-
 }
 
-void
-Sight::update_self()
-{
+void Sight::update_self(){
     set_x(this->x());
     set_y(this->y());
 }

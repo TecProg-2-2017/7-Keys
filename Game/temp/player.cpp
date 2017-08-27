b2bb0c0ca0ba2a6476a1472125b23d8e9e1d7179
Arthur Assis <arthur120496@gmail.com>
2017-08-27 16:58:38 -0300

    Applying stylesheet

--- Game/temp/player.cpp
+++ Game/temp/player.cpp
@@ -178,193 +178,163 @@ bool Player::on_event(const KeyboardEvent& event){
   return false;
 }
 
-void
-Player::draw_self()
-{
-    Environment * env = Environment::get_instance();
+void Player::draw_self(){
+  Environment *env = Environment::get_instance();
 
-    Rect darkness {0,0,1280,720};
-    Color m_fad, m_fad2;
-    m_fad.set_a(230);
-    env->canvas->set_blend_mode(Canvas::BLEND);
-    env->canvas->fill(darkness, m_fad);
+  Rect darkness {0,0,1280,720};
+  Color m_fad, m_fad2;
+  m_fad.set_a(230);
+  env->canvas->set_blend_mode(Canvas::BLEND);
+  env->canvas->fill(darkness, m_fad);
 
-    Circle self_light{{x() + 30, y() + 30},100};
-    m_fad2 = Color::WHITE;
-    m_fad2.set_a(30);
-    env->canvas->fill(self_light, m_fad2);
+  Circle self_light{{x() + 30, y() + 30},100};
+  m_fad2 = Color::WHITE;
+  m_fad2.set_a(30);
+  env->canvas->fill(self_light, m_fad2);
 
-    m_states[m_state]->draw_self();
+  m_states[m_state]->draw_self();
 }
 
-void
-Player::update_self(unsigned long elapsed)
-{
-    m_states[m_state]->update_self(elapsed);
+void Player::update_self(unsigned long elapsed){
+  m_states[m_state]->update_self(elapsed);
 
-    if (not m_last)
-    {
-        m_last = elapsed;
-    }
-
-    double dx = m_left*(-SPEED) + m_right*SPEED;
-    double x = this->x() + dx*((elapsed - m_last)/1000.0);
-
-    double dy = m_up*(-SPEED) + m_down*SPEED;
-    double y = this->y() + dy*((elapsed - m_last)/1000.0);
-
+  if (not m_last){
     m_last = elapsed;
+  }
 
-    if (x < 0)
-    {
-        x = 0;
+  double dx = m_left*(-SPEED) + m_right*SPEED;
+  double x = this->x() + dx*((elapsed - m_last)/1000.0);
+
+  double dy = m_up*(-SPEED) + m_down*SPEED;
+  double y = this->y() + dy*((elapsed - m_last)/1000.0);
+
+  m_last = elapsed;
+
+  if (x < 0){
+    x = 0;
+  }
+
+  Environment *env = Environment::get_instance();
+  if (x + w() > env->canvas->w()){
+    x = env->canvas->w() - w();
+  }
+
+  if ((x == env->canvas->w() - w() and dx > 0) or (x == 0 and dx < 0)){
+    change_state(Player::IDLE, m_state);
+  }
+
+  if (y < 0){
+    y = 0;
+  }
+
+  if (y + h() > env->canvas->h()){
+    y = env->canvas->h() - h();
+  }
+
+  if ((y == env->canvas->h() - h() and dy > 0) or (y == 0 and dy < 0)){
+    change_state(Player::IDLE, m_state);
+  }
+
+  set_x(x);
+  set_y(y);
+
+  /* Colisao com as portas */
+  int posx,posy;
+  posx = (int) x;
+  posy = (int) y;
+
+  if(posx <= 1 && ( posy >= 280 && posy <= 420) &&
+    current_map->current_room->r_left){
+      enter_room(current_map->current_room, current_map->current_room->r_left, 1120, posy);
+  }
+  else if(posx >= 1200 && ( posy >= 280 && posy <= 420) &&
+    current_map->current_room->r_right){
+      enter_room(current_map->current_room,
+      current_map->current_room->r_right, 80, posy);
+  }
+  else if(posy <= 1  && ( posx >= 600 && posx <= 680) &&
+    current_map->current_room->r_top){
+      enter_room(current_map->current_room, current_map->current_room->r_top,
+        posx, 580);
+  }
+  else if(posy >= 620  && ( posx >= 600 && posx <= 680) &&
+    current_map->current_room->r_bottom){
+      enter_room(current_map->current_room,
+        current_map->current_room->r_bottom, posx, 80);
+  }
+
+  /*Colisoes com os items */
+  vector <Item*> aux = current_map->current_room->items;
+
+  for(int i = 0; i < aux.size(); i++){
+    if(aux[i]->type == "key"){
+      if (((posx + 40 > aux[i]->x()) && (posx + 40 < (aux[i]->x() + 32))) &&
+        ((posy + 40> aux[i]->y()) && (posy + 40 < (aux[i]->y() + 32)))){
+          current_map->current_room->remove_child(aux[i]);
+          cout << "peguei a chave" << endl;
+          pick_key();
+      }
     }
+    else if(aux[i]->type == "finaldoor"){
+      if(has_key()){
+        if (((posx + 40 > aux[i]->x()) && (posx + 40 < (aux[i]->x() + 80))) &&
+          ((posy + 40> aux[i]->y()) && (posy + 40 < (aux[i]->y() + 80)))){
+            cout << "você ganhou o jogo!" << endl;
+            drop_key();
 
-    Environment *env = Environment::get_instance();
-    if (x + w() > env->canvas->w())
-    {
-        x = env->canvas->w() - w();
-    }
-
-    if ((x == env->canvas->w() - w() and dx > 0) or
-        (x == 0 and dx < 0))
-    {
-        change_state(Player::IDLE, m_state);
-    }
-
-    if (y < 0)
-    {
-        y = 0;
-    }
-
-    if (y + h() > env->canvas->h())
-    {
-        y = env->canvas->h() - h();
-    }
-
-    if ((y == env->canvas->h() - h() and dy > 0) or
-        (y == 0 and dy < 0))
-    {
-        change_state(Player::IDLE, m_state);
-    }
-
-    set_x(x);
-    set_y(y);
-
-
-    /* Colisao com as portas */
-    int posx,posy;
-    posx = (int) x;
-    posy = (int) y;
-
-    if(posx <= 1 && ( posy >= 280 && posy <= 420) && current_map->current_room->r_left)
-    {
-
-        enter_room(current_map->current_room, current_map->current_room->r_left, 1120, posy);
-    }
-    else if(posx >= 1200 && ( posy >= 280 && posy <= 420) && current_map->current_room->r_right)
-    {
-
-        enter_room(current_map->current_room, current_map->current_room->r_right, 80, posy);
-    }
-    else if(posy <= 1  && ( posx >= 600 && posx <= 680) && current_map->current_room->r_top)
-    {
-
-        enter_room(current_map->current_room, current_map->current_room->r_top, posx, 580);
-    }
-    else if(posy >= 620  && ( posx >= 600 && posx <= 680) && current_map->current_room->r_bottom)
-    {
-
-        enter_room(current_map->current_room, current_map->current_room->r_bottom, posx, 80);
-    }
-
-
-
-    /*Colisoes com os items */
-    vector <Item*> aux = current_map->current_room->items;
-
-    for(int i = 0; i < aux.size(); i++)
-    {
-        if(aux[i]->type == "key")
-        {
-            if (((posx + 40 > aux[i]->x()) && (posx + 40 < (aux[i]->x() + 32))) && ((posy + 40> aux[i]->y()) && (posy + 40 < (aux[i]->y() + 32))))
-            {
-                current_map->current_room->remove_child(aux[i]);
-                cout << "peguei a chave" << endl;
-                pick_key();
-            }
+            //Passa pro prox level
+            Level *next_level = new Level("stage","stage2");
+            next_level->set_next("stage2");
         }
-        else if(aux[i]->type == "finaldoor")
-        {
-            if(has_key())
-            {
-                if (((posx + 40 > aux[i]->x()) && (posx + 40 < (aux[i]->x() + 80))) && ((posy + 40> aux[i]->y()) && (posy + 40 < (aux[i]->y() + 80))))
-                {
-                    cout << "você ganhou o jogo!" << endl;
-                    drop_key();
-                    //Passa pro prox level
-                    Level *next_level = new Level("stage","stage2");
-                    next_level->set_next("stage2");
-                }
-            }
-        }
+      }
     }
+  }
 }
 
-void
-Player::enter_room(Room * anterior, Room * nova, int posx, int posy)
-{
-    set_position(posx, posy);
-    current_map->enter_room(anterior, nova);
-
+void Player::enter_room(Room * anterior, Room * nova, int posx, int posy){
+  set_position(posx, posy);
+  current_map->enter_room(anterior, nova);
 }
 
-void
-Player::report_event(Event event)
-{
-    State next = m_fst[m_state][event];
+void Player::report_event(Event event){
+  State next = m_fst[m_state][event];
 
-    if (next != m_state and next != NONE)
-    {
-        change_state(next, m_state);
-        m_last = 0;
-    }
+  if (next != m_state and next != NONE){
+    change_state(next, m_state);
+    m_last = 0;
+  }
 }
 
-void
-Player::change_state(State to, State)
-{
-    m_state = to;
+void Player::change_state(State to, State){
+  m_state = to;
 }
 
-short
-Player::direction() const
-{
-    if(m_left)
-        return 0;
-    else if(m_up)
-        return 1;
-    else if(m_right)
-        return 2;
-    else if(m_down)
-        return 3;
-    else
-        return -1;
+short Player::direction() const{
+  if(m_left){
+    return 0;
+  }
+  else if(m_up){
+    return 1;
+  }
+  else if(m_right){
+    return 2;
+  }
+  else if(m_down){
+    return 3;
+  }
+  else{
+    return -1;
+  }
 }
 
-bool
-Player::has_key()
-{
-    return key;
+bool Player::has_key(){
+  return key;
 }
 
-void
-Player::pick_key()
-{
-    this->key = true;
+void Player::pick_key(){
+  this->key = true;
 }
-void
-Player::drop_key()
-{
-    this->key = false;
+
+void Player::drop_key(){
+  this->key = false;
 }
